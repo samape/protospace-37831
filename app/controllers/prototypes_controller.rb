0d51a59b7f98d068c073cabdb28365bda8d0c18f
samape <s.sakamoto.613@gmail.com>
2022-07-09 17:26:50 +0900

    プロトタイプ編集機能の修正
    自身の投稿したプロトタイプの詳細ページから編集ボタンをクリックした際に、編集ページへ遷移できるように修正。

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -27,7 +27,7 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in? == current_user.name
+    unless user_signed_in? && current_user.name == @prototype.user.name
       redirect_to action: :index
     end
   end
